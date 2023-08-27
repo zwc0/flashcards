528920096e7b9ab4b1e25d5e244b901596dd1913
Austin S <acceptance364@gmail.com>
2023-08-26 19:07:44 -0700

    attempt mobile fix

--- src/app.tsx
+++ src/app.tsx
@@ -1,8 +1,25 @@
 import {render} from 'preact';
 import Home from './pages/Home';
 import PWABanner from './components/PWABanner';
+import { useEffect } from 'preact/hooks';
+
+let height = 0;
+async function updateScreenHeight(){
+    const h = window.innerHeight;
+    if (!h || h === height)
+        return;
+    height = h;
+    document.body.style.setProperty('--screen-h', `${h-1}px`);
+}
 
 const App = () => {
+    useEffect(()=>{
+        const interval = setInterval(()=>{
+            updateScreenHeight();
+        }, 1000);
+        return ()=>clearInterval(interval);
+    }, []);
+
     return (<>
         <Home />
         <PWABanner />
